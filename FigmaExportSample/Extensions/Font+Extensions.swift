b1722ca996d8608cf067be07563cceab8aa27dd8
Max Gribox <gribov.max@gmail.com>
2022-01-14 15:59:24 +0300

    figma-export typography basic

--- FigmaExportSample/Extensions/Font+Extensions.swift
+++ FigmaExportSample/Extensions/Font+Extensions.swift
@@ -1,8 +1,30 @@
+//  swiftlint:disable all
 //
-//  Font+Extensions.swift
-//  FigmaExportSample
+//  The code generated using FigmaExport — Command line utility to export
+//  colors, typography, icons and images from Figma to Xcode project.
 //
-//  Created by Max Gribov on 14.01.2022.
+//  https://github.com/RedMadRobot/figma-export
+//
+//  Don’t edit this code manually to avoid runtime crashes
 //
 
 import SwiftUI
+
+public extension Font {
+    
+    static func heading() -> Font {
+        Font.custom("SFProDisplay-Semibold", size: 24.0)
+    }
+    static func heading2() -> Font {
+        Font.custom("SFProDisplay-Semibold", size: 18.0)
+    }
+    static func heading3() -> Font {
+        Font.custom("SFProDisplay-Semibold", size: 16.0)
+    }
+    static func normal() -> Font {
+        Font.custom("SFProDisplay-Regular", size: 14.0)
+    }
+    static func promo() -> Font {
+        Font.custom("SFProDisplay-Semibold", size: 36.0)
+    }
+}
