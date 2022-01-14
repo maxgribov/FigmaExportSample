10995e30867115778658c80e4135ca107bec0753
Max Gribox <gribov.max@gmail.com>
2022-01-14 13:29:24 +0300

    Initial Commit

---
+++ FigmaExportSample/FigmaExportSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaExportSampleApp.swift
+//  FigmaExportSample
+//
+//  Created by Max Gribov on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct FigmaExportSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
