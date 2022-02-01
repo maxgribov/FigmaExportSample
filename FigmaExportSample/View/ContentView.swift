fb291cdc2b84f18c9660c8a9ac4b2f04692015f0
Max Gribox <gribov.max@gmail.com>
2022-02-01 09:58:46 +0300

    Update

--- FigmaExportSample/View/ContentView.swift
+++ FigmaExportSample/View/ContentView.swift
@@ -93,6 +93,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         
         ContentView(viewModel: .sample)
-            .preferredColorScheme(.light)
+            .preferredColorScheme(.dark)
     }
 }
